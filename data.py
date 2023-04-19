41903e6967249a006ee8f37a232087d614e93717
v4zha <arvin_j4848@protonmail.com>
2023-04-20 03:00:01 +0530

    multi-threading

--- data.py
+++ data.py
@@ -1,31 +1,34 @@
 from ear_utils import DataProc
+from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 import pandas as pd
 
 
-def main():
-    # Saving fake data
-    fk_data = []
-    for vid_id in tqdm(range(0, 1000)):
-        dp = DataProc(f"./vid_data/fake/{vid_id}.mp4")
-        blink_count = dp.get_blink_count()
-        frames = len(dp)
-        bpf = blink_count/frames
-        fk_data.append({"vid_id": vid_id, "blink_count": blink_count,
-                        "frames": frames, "bpf": bpf})
-    df = pd.DataFrame(fk_data)
-    df.to_csv("fake_data.csv", index=False)
+def proc_vid(vid_id, vid_type):
+    dp = DataProc(f"./vid_data/{vid_type}/{vid_id}.mp4")
+    blink_count = dp.get_blink_count()
+    frames = len(dp)
+    bpf = blink_count/frames
+    return {"vid_id": vid_id, "blink_count": blink_count, "frames": frames, "bpf": bpf}
 
-    real_data = []
-    for vid_id in tqdm(range(0, 1000)):
-        dp = DataProc(f"./vid_data/original/{vid_id}.mp4")
-        blink_count = dp.get_blink_count()
-        frames = len(dp)
-        bpf = blink_count/frames
-        real_data.append({"vid_id": vid_id, "blink_count": blink_count,
-                          "frames": frames, "bpf": bpf})
-    df = pd.DataFrame(real_data)
-    df.to_csv("orginial_data.csv", index=False)
+
+def main():
+    with ThreadPoolExecutor() as executor:
+        # Saving fake data
+        fake_fut = [executor.submit(
+            proc_vid, vid_id, "fake") for vid_id in range(1)]
+        fk_data = [f.result()
+                   for f in tqdm(fake_fut, total=len(fake_fut))]
+        df = pd.DataFrame(fk_data)
+        df.to_csv("fake_data.csv", index=False)
+
+        # Saving original data
+        orig_fut = [executor.submit(
+            proc_vid, vid_id, "original") for vid_id in range(1)]
+        orig_data = [f.result()
+                     for f in tqdm(orig_fut, total=len(orig_fut))]
+        df = pd.DataFrame(orig_data)
+        df.to_csv("original_data.csv", index=False)
 
 
 if __name__ == "__main__":
